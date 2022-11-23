85627c2832fe50b67d335fcaeec867f33f38eff7
elmaboussoutigue <elmaboussoutigue@els-macbook-air.fios-router.home>
2022-11-23 11:01:15 -0500

    nov 23 upload

--- TechShop_Project/src/main/java/com/smarttechqa/base/BaseClassTec.java
+++ TechShop_Project/src/main/java/com/smarttechqa/base/BaseClassTec.java
@@ -1,46 +1,86 @@
 package com.smarttechqa.base;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.smarttechqa.utilities.UtilitiesTec;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
-public class BaseClassTec {
 
 
-	public static void setUp(){
 
-		//my git hub token:            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
+public class BaseClassTec  {
 
-		//System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");  ===>for window PC
-	//	System.setProperty("webdriver.chrome.driver", "./Application /chromedriver");  //===>for Mac
-	
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+	public static WebDriver driver;
+	public static void setUp() throws IOException {
+		//System.setProperty("webdriver.chrome.driver", "./Application /chromedriver"); 
+	    WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
         driver.manage().window().maximize();
-		driver.get("https://www.smarttechqa.com/login");
-		
-		
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);// use for the all dom page to wait. it is part of selenium challenge
+        driver.get("https://www.smarttechqa.com/");
+        
+        
+        
+        
+        UtilitiesTec.getExplicitWait( driver.findElement(By.xpath("//a[@href='/login']")), 5);
+        driver.findElement(By.xpath("//a[@href='/login']")).click();
+
 		//driver.findElement(By.id("email")).sendKeys("testuser@gmail.com");
+        
+        
 		driver.findElement(By.xpath("//*[@id='email']")).sendKeys("testuser@email.com");
-		
+		UtilitiesTec.getHighLighter(driver.findElement(By.xpath("//*[@id='email']")));
+		UtilitiesTec.takeScreenShot();
 		//driver.findElement(By.id("password")).sendKeys("123456");
-		
 		driver.findElement(By.xpath("//*[@type='password']")).sendKeys("123456");
-		
+		UtilitiesTec.getExplicitWait(driver.findElement(By.xpath("//*[@class='btn btn-primary']")), 5);
 		driver.findElement(By.xpath("//*[@class='btn btn-primary']")).click();
-		
 		System.out.println(driver.getTitle()); 
+		//Thread.sleep(5000);
 		
-		//driver.quit();
 		
-	
-	
-	
+		
+		//driver.quit(); 
+		UtilitiesTec.getExplicitWait(driver.findElement(By.xpath("//a[@href='/cart']")), 5);
+		driver.findElement(By.xpath("//a[@href='/cart']")).click();
+		//driver.findElement(By.linkText("Go Back")).click(); //this will take me back to the home page from the cart page
+		driver.findElement(By.partialLinkText("Go")).click();
+		
+		
+		driver.findElement(By.name("q")).sendKeys("iphone"); //Sends keys to the searchbox
+		//driver.findElement(By.cssSelector(".p-2 btn btn-outline-success")).click();
+		//Thread.sleep(5000);
+		UtilitiesTec.getExplicitWait(driver.findElement(By.xpath("//*[text()='Search']")), 5);
+		driver.findElement(By.xpath("//*[text()='Search']")).click();
+		
+		driver.navigate().back(); //This takes to the previous page.
+		driver.navigate().refresh();//this refesh the page 
+		//driver.navigate().to("https://www.smarttechqa.com");//same thjing as line 19
+		
+		driver.quit(); 
+		
+		
 	}
 	
 	
-	public static void main(String[] args) {
+	
+	
+	
+	
+	
+      public static void main(String[] args) throws IOException  {
+		
 		BaseClassTec.setUp();
-	}
+	
+      }
+	
+	
 	
 	
 	
